b8f044a8afa241aefff7aa3ac42508d0cab76231
DonnerNW School Acc <61599222+WHomegames@users.noreply.github.com>
2023-05-17 00:51:07 +0000

    Fixed rendering setting

--- game/VisualHelper/World.java
+++ game/VisualHelper/World.java
@@ -87,8 +87,14 @@ public class World extends JFrame {
 		og.setBackground(new Color(0,0,0,0));
 		gg.setBackground(backgroundColor);
 		
+		//Set rendering hints: Basicaly the renderers preferences
+
 		Graphics2D[] gs = {og, gg};
 		for(Graphics2D g : gs) {
+
+			//no filter
+			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+
 			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 			g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
 			g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
